c43e54b2d5cde27ce1abb1fe995a871287be8b37
BrendanJones44 <bpj1651@rit.edu>
2017-03-18 12:46:18 -0400

    Skeleton LibraryBookSearch

---
+++ src/LibraryBookSearchRequest.java
@@ -0,0 +1,19 @@
+
+public class LibraryBookSearchRequest implements Request {
+
+    /* The LBMS itself so execute can call commands */
+    private LBMS lbms;
+
+    public LibraryBookSearchRequest(){
+
+    }
+
+    public void execute(){
+        //TODO
+    }
+
+    public String response(){
+        return null;
+        //TODO
+    }
+}
