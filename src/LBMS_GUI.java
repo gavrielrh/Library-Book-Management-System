f101541df901e9a9ea370133f20ed37c962b6c37
Junwen Mai <jxm7861@g.rit.edu>
2017-04-17 14:19:47 -0400

    Updated GUI

--- src/LBMS_GUI.java
+++ src/LBMS_GUI.java
@@ -3,18 +3,18 @@
  */
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -22,22 +22,25 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 
 public class LBMS_GUI extends Application {
-
+    private Stage stage;
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
+        stage = primaryStage;
         primaryStage.setTitle("LBMS");
 
         primaryStage.setScene(login());
         primaryStage.show();
     }
 
-    public static Scene login(){
+    public Scene login(){
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
@@ -71,7 +74,7 @@ public class LBMS_GUI extends Application {
 
             @Override
             public void handle(ActionEvent e) {
-                //handle login
+                stage.setScene(VisitorAccount());
             }
         });
 
@@ -81,23 +84,104 @@ public class LBMS_GUI extends Application {
         return scene;
     }
 
-    public static Scene request(){
+
+
+    public Scene VisitorAccount(){
         BorderPane pane = new BorderPane();
 
-        HBox hbox = new HBox();
-        hbox.setPadding(new Insets(10,10,10,10));
-        hbox.setSpacing(10);
-        hbox.setStyle("-fx-background-color: red");
+        HBox borrowBooks = new HBox();
+        borrowBooks.setPadding(new Insets(20, 20, 0, 20));
+        borrowBooks.setAlignment(Pos.BASELINE_RIGHT);
+        Button borrowBook = new Button("Borrow Books");
+        borrowBooks.getChildren().add(borrowBook);
 
-        Text command = new Text("Commands");
-        TextField commandText = new TextField();
-        commandText.setPrefWidth(500 - command.getWrappingWidth());
+        borrowBook.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                stage.setScene(VisitorBorrow());
+            }
+        });
 
-        hbox.getChildren().addAll(command, commandText);
+        //Borrowed Books List
+        BorderPane borrow = new BorderPane();
+        borrow.setPadding(new Insets(20,20,20,20));
+        Text booksBorrowed = new Text("Books Borrowed");
+        ListView<String> books = new ListView<>();
+        ObservableList<String> b = FXCollections.observableArrayList();
+        b.add("book 1");
+        b.add("book 2");
+        b.add("book 3");
+        books.setItems(b);
+        books.setPrefWidth(450);
+        books.setPrefHeight(350);
+        Button bookReturn = new Button("Return Book");
+        borrow.setTop(booksBorrowed);
+        borrow.setCenter(books);
+        borrow.setBottom(bookReturn);
 
-        pane.setBottom(hbox);
+        //
+
+        VBox fines = new VBox();
+        fines.setPadding(new Insets(0,20,20,20));
+        Text fine = new Text("Fines: S" + 5.00);
+        Button pay = new Button("Pay Fine");
+        fines.getChildren().addAll(fine, pay);
+
+        pane.setTop(borrowBooks);
+        pane.setCenter(borrow);
+        pane.setBottom(fines);
         Scene scene = new Scene(pane, 500,500);
         return scene;
     }
 
+    public Scene VisitorBorrow(){
+        BorderPane pane = new BorderPane();
+        //search
+        HBox search = new HBox();
+        search.setSpacing(10);
+        search.setPadding(new Insets(20,20,20,20));
+        ObservableList<String> options =
+                FXCollections.observableArrayList(
+                        "Type 1",
+                        "Type 2",
+                        "Type 3"
+                );
+        ComboBox<String> searchType = new ComboBox<>(options);
+        TextField searchInput = new TextField("search");
+        Button searchBtn = new Button("Search");
+        search.getChildren().addAll(searchType, searchInput, searchBtn);
+
+        //results
+        VBox results = new VBox();
+        results.setPadding(new Insets(20,20,20,20));
+        results.setPrefHeight(200);
+        Text resultText = new Text("Results");
+        ListView<String> result = new ListView<>();
+        ObservableList<String> r = FXCollections.observableArrayList();
+        r.add("result 1");
+        r.add("result 2");
+        r.add("result 3");
+        result.setItems(r);
+        results.getChildren().addAll(resultText, result);
+
+        //checkout
+        VBox checkout = new VBox();
+        checkout.setPadding(new Insets(20,20,20,20));
+        checkout.setPrefHeight(200);
+        Text booklist = new Text("Books to checkout");
+        ListView<String> books = new ListView<>();
+        ObservableList<String> b = FXCollections.observableArrayList();
+        b.add("book 1");
+        b.add("book 2");
+        b.add("book 3");
+        books.setItems(b);
+        Button checkoutBtn = new Button("Checkout");
+
+        checkout.getChildren().addAll(booklist, books, checkoutBtn);
+        pane.setTop(search);
+        pane.setCenter(results);
+        pane.setBottom(checkout);
+        Scene scene = new Scene(pane, 500, 500);
+        return scene;
+    }
 }
